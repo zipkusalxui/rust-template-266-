066c900246a2df66baecd8f8869aff056538953f
skulidropek <66840575+skulidropek@users.noreply.github.com>
2024-11-25 23:13:55 +0400

    Init

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("HelloWorld", "PublicRust", "1.0.0")]
+    [Description("Simple Hello World plugin for Rust")]
+
+    public class HelloWorld : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("Hello, Rust World! Plugin successfully loaded.");
+        }
+
+        [ChatCommand("hello")]
+        private void HelloCommand(IPlayer player, string command, string[] args)
+        {
+            player.Reply("Hello, " + player.Name + "! Welcome to this Rust server.");
+        }
+    }
+}
